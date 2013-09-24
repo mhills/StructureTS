e3b410f9e72c601ebb2bbdf8575b46ef6d0701ff
Robert S <rsavian@nerdery.com>
2013-09-24 09:04:42 -0500

    Add Docs

---
+++ docs/api.js
@@ -0,0 +1,142 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ApplicationCacheController",
+        "ApplicationCacheEvent",
+        "BaseController",
+        "BaseEvent",
+        "BaseObject",
+        "BaseRequest",
+        "BaseTransition",
+        "BaseViewController",
+        "Bitmap",
+        "BrowserUtils",
+        "BulkLoader",
+        "Canvas",
+        "CanvasElement",
+        "CarouselComponent",
+        "CarouselEvent",
+        "Collection",
+        "DOMElement",
+        "DataLoader",
+        "DisplayObjectContainer",
+        "EventBroker",
+        "EventDispatcher",
+        "HtmlLoader",
+        "ICollection",
+        "ICore",
+        "IDOMElement",
+        "IDataStore",
+        "IDisplayObject",
+        "ITransition",
+        "IValueObject",
+        "IViewController",
+        "ImageLoader",
+        "LanguageConfigVO",
+        "LanguageEvent",
+        "LanguageModel",
+        "LoaderEvent",
+        "LocalStorageController",
+        "LocalStorageEvent",
+        "MobileUtil",
+        "MouseEvents",
+        "NavigatorEvents",
+        "NetworkMonitor",
+        "NetworkMonitorEvent",
+        "NumberUtil",
+        "RequestEvent",
+        "RouterController",
+        "RouterEvent",
+        "Stage",
+        "StringUtil",
+        "TabComponent",
+        "TemplateFactory",
+        "TimeUtil",
+        "Timer",
+        "TimerEvent",
+        "TransitionCrossFade",
+        "TransitionFactory",
+        "TransitionFadeOutAndIn",
+        "TransitionManager",
+        "TransitionManagerEvent",
+        "TransitionNone",
+        "TransitionPushDown",
+        "TransitionPushLeft",
+        "TransitionPushRight",
+        "TransitionPushUp",
+        "TransitionType",
+        "TweenEvent",
+        "URLContentType",
+        "URLLoader",
+        "URLLoaderDataFormat",
+        "URLRequest",
+        "URLRequestMethod",
+        "Util",
+        "ValueObject",
+        "WindowEvents"
+    ],
+    "modules": [
+        "StructureTS",
+        "controller",
+        "core",
+        "event",
+        "interface",
+        "model",
+        "nativeEvent",
+        "net",
+        "util",
+        "view"
+    ],
+    "allModules": [
+        {
+            "displayName": "controller",
+            "name": "controller",
+            "description": "YUIDoc_comment"
+        },
+        {
+            "displayName": "core",
+            "name": "core",
+            "description": "The {{#crossLink \"BaseObject\"}}{{/crossLink}} class is an abstract class that provides common properties and functionality for all StructureTS classes."
+        },
+        {
+            "displayName": "event",
+            "name": "event",
+            "description": "The ApplicationCacheEvent ...."
+        },
+        {
+            "displayName": "interface",
+            "name": "interface",
+            "description": "YUIDoc_comment"
+        },
+        {
+            "displayName": "model",
+            "name": "model",
+            "description": "YUIDoc_comment"
+        },
+        {
+            "displayName": "nativeEvent",
+            "name": "nativeEvent",
+            "description": "The MouseEvents..."
+        },
+        {
+            "displayName": "net",
+            "name": "net",
+            "description": "The URLContentType..."
+        },
+        {
+            "displayName": "StructureTS",
+            "name": "StructureTS"
+        },
+        {
+            "displayName": "util",
+            "name": "util",
+            "description": "The BrowserUtils..."
+        },
+        {
+            "displayName": "view",
+            "name": "view",
+            "description": "The Bitmap..."
+        }
+    ]
+} };
+});
