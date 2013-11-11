27c308875154ac2d70852e7e9a9f6d094d9611d2
codebelt <code@codebelt.com>
2013-11-10 19:41:21 -0600

    clean up code.

--- examples/Phonegap/dev/scripts/_compiled/templates.tmpl.js
+++ examples/Phonegap/dev/scripts/_compiled/templates.tmpl.js
@@ -6,7 +6,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   
 
 
-  return "<div>\r\n    <img src=\"img/header.png\" width=\"100%\"/>\r\n    <br/>\r\n    <button type=\"button\" id=\"js-addTodoButton\" class=\"addToDo\"><img src=\"img/button_addtodo.png\"/></button>\r\n    <button type=\"button\" id=\"js-removeTasksButton\" class=\"removeTasks\"><img src=\"img/button_removetasks.png\"/></button>\r\n    <br/><br/><br/>\r\n    <table id=\"js-todoContainer\" width=\"100%\" border=\"0\">\r\n    </table>\r\n</div>\r\n";
+  return "<div>\n    <img src=\"img/header.png\" width=\"100%\"/>\n    <br/>\n    <button type=\"button\" id=\"js-addTodoButton\" class=\"addToDo\"><img src=\"img/button_addtodo.png\"/></button>\n    <button type=\"button\" id=\"js-removeTasksButton\" class=\"removeTasks\"><img src=\"img/button_removetasks.png\"/></button>\n    <br/><br/><br/>\n    <table id=\"js-todoContainer\" width=\"100%\" border=\"0\">\n    </table>\n</div>\n";
   });
 
 this["JST"]["templates/PhoneGapTemplate.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
@@ -15,7 +15,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   
 
 
-  return "<div class=\"app\">\r\n    <h1>PhoneGap</h1>\r\n    <div id=\"deviceready\" class=\"blink\">\r\n        <p class=\"event listening\">Connecting to Device</p>\r\n        <p class=\"event received\">Device is Ready</p>\r\n    </div>\r\n</div>";
+  return "<div class=\"app\">\n    <h1>PhoneGap</h1>\n    <div id=\"deviceready\" class=\"blink\">\n        <p class=\"event listening\">Connecting to Device</p>\n        <p class=\"event received\">Device is Ready</p>\n    </div>\n</div>";
   });
 
 this["JST"]["templates/TodoItemTemplate.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
@@ -32,13 +32,13 @@ function program1(depth0,data) {
 function program3(depth0,data) {
   
   
-  return "\r\n        <td><input type=\"checkbox\" checked name=\"\" class=\"checkbox\"></td>\r\n    ";
+  return "\n        <td><input type=\"checkbox\" checked name=\"\" class=\"checkbox\"></td>\n    ";
   }
 
 function program5(depth0,data) {
   
   
-  return "\r\n        <td><input type=\"checkbox\" name=\"\" class=\"checkbox\"></td>\r\n    ";
+  return "\n        <td><input type=\"checkbox\" name=\"\" class=\"checkbox\"></td>\n    ";
   }
 
   buffer += "<tr data-id=\""
@@ -46,11 +46,11 @@ function program5(depth0,data) {
     + "\" class=\"todoItem ";
   stack2 = helpers['if'].call(depth0, depth0.completed, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack2 || stack2 === 0) { buffer += stack2; }
-  buffer += "\">\r\n    ";
+  buffer += "\">\n    ";
   stack2 = helpers['if'].call(depth0, depth0.completed, {hash:{},inverse:self.program(5, program5, data),fn:self.program(3, program3, data),data:data});
   if(stack2 || stack2 === 0) { buffer += stack2; }
-  buffer += "\r\n    <td><input type=\"text\" name=\"\" class=\"textbox\" value=\""
+  buffer += "\n    <td><input type=\"text\" name=\"\" class=\"textbox\" value=\""
     + escapeExpression(((stack1 = depth0.text),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\"></td>\r\n    <td><input type=\"button\" class=\"viewButton\"></td>\r\n    <td><input type=\"button\" class=\"deleteButton\"></td>\r\n</tr>";
+    + "\"></td>\n    <td><input type=\"button\" class=\"viewButton\"></td>\n    <td><input type=\"button\" class=\"deleteButton\"></td>\n</tr>";
   return buffer;
   });
